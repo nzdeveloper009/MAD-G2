aea67701b9478a5eb7e03c8affd65d2ef332621a
Hafsa Noor <hafsanoor34202@gmail.com>
2026-04-25 01:37:59 +0500

    basic print arithmetic operations

---
+++ hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/L1-Function.kt
@@ -0,0 +1,12 @@
+package com.example.learnkotlin
+
+fun main() {
+    println("welcome to kotlin")
+    var a =45
+    var b = 9
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+}
