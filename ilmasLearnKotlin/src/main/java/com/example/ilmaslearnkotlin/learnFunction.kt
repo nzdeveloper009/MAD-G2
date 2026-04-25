cb48fc141fee59bd172fb8dd55a9fe08ea2bddf4
Ilmas shabir <almasshabir967@gmail.com>
2026-04-25 21:23:10 +0500

    Functions

---
+++ ilmasLearnKotlin/src/main/java/com/example/ilmaslearnkotlin/learnFunction.kt
@@ -0,0 +1,11 @@
+package com.example.ilmaslearnkotlin
+fun main(){
+    println("hello world " )
+    println(1 )
+    println(false)
+    // println move control on the next line but
+// simple print statement print next statement on the same line.
+    print("hello world")
+    print(1)
+    print(false)
+}
