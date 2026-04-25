62e836396b6259918f887f56e014f34968a7462c
Hafsa Noor <hafsanoor34202@gmail.com>
2026-04-25 15:43:02 +0500

    basic string builtIn funtions

--- hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/L1-Function.kt
+++ hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/L1-Function.kt
@@ -1,26 +1,46 @@
 package com.example.learnkotlin
 
 fun main() {
-    println("welcome to kotlin")
-    val a =45
-    val b = 9
+//    println("welcome to kotlin")
+//    val a =45
+//    val b = 9
+//
+//    // arithmetic operators
+//    println(a+b)
+//    println(a-b)
+//    println(a*b)
+//    println(a/b)
+//
+//    // relational operators
+//    println(a>b)
+//    println(a<b)
+//    println(a<=b)
+//    println(a>=b)
+//    println(a==b)
+//    println(a!=b)
+//
+//    // logical operators
+//    println(a==b||a>b)
+//    println(a!=b&& a>b)
 
-    // arithmetic operators
-    println(a+b)
-    println(a-b)
-    println(a*b)
-    println(a/b)
+    // some builtIn fun of string
+    var name = "Hafsa Noor"
+    println(name)
 
-    // relational operators
-    println(a>b)
-    println(a<b)
-    println(a<=b)
-    println(a>=b)
-    println(a==b)
-    println(a!=b)
+    println(name[6])   // specific element
+    println("third element: " + name[2])
 
-    // logical operators
-    println(a==b||a>b)
-    println(a!=b&& a>b)
+    var rollNo = "24011519-151"
+    println(name + rollNo )   // add two strings
+    println("characters in RollNo: " + rollNo.length) // length
+    println(name.uppercase()) // HAFSA NOOR
+    println(name.lowercase())  //hafsa noor
+
+    println(name.compareTo("Hafsa Noor"))  // for compare 2 strings
+    println(rollNo.indexOf("519"))  //it returns the index of the first occurrence of a specified text
+
+    println("My name $name and rollNo is $rollNo") //template
+
+
 
 }
