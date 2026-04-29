2ea859b535a9f831821069e8b076e60e615f359e
Hafsa Noor <hafsanoor34202@gmail.com>
2026-04-29 23:51:29 +0500

    Constructor and init

--- hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/Clsss&Objects.kt
+++ hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/Clsss&Objects.kt
@@ -9,10 +9,24 @@ fun main() {
     s1.upload()
     s2.payfee()
 
+    var s3= Student("Ayesha") // Secondary constructor only call by this object
+
+    val person= Person("Ali", 18)
+    println(person.name)
+    println(person.isVote)
 }
 
 class Student(var name: String, var age:Int, var dept:String ){
 
+    // init block = constructor logic (auto call hota hai)
+    init {
+        println("Auto call for $name")
+    }
+
+    // Secondary constructor
+    constructor(name: String) : this(name, 18, "Unknown") {
+        println("Secondary constructor called for $name")
+    }
     fun upload(){
         println("$name uploaded an assignment")
     }
@@ -21,4 +35,11 @@ class Student(var name: String, var age:Int, var dept:String ){
         println("name: $name  department: $dept  fee is paid")
     }
 
+}
+
+// in this type of primary constructor we can apply som operation on the parameters it behaves like function
+class Person(nameparam:String,p_age:Int){
+    val name="$nameparam.clan"
+    val age=p_age
+    val isVote:Boolean=age>18
 }
