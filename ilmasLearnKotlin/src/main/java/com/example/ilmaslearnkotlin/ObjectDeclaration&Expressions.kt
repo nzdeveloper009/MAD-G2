2d76f32a86b840e9ad4fd84c6bf2abde99b26411
Ilmas shabir <almasshabir967@gmail.com>
2026-05-01 10:25:37 +0500

    Object Declaration , Object Expression ,companion object and factory Pattern

--- ilmasLearnKotlin/src/main/java/com/example/ilmaslearnkotlin/ObjectDeclaration&Expressions.kt
+++ ilmasLearnKotlin/src/main/java/com/example/ilmaslearnkotlin/ObjectDeclaration&Expressions.kt
@@ -14,13 +14,29 @@ println("Singleton Object")
     println("--------------------")
     //Anonymous object---object without name
     // object expression
-    val obj = object : Animal() {
-        override fun sound() {
-            println("Custom sound")
+    val button = object : ClickListener {
+        override fun onClick() {
+            println("Button clicked")
         }
     }
 
-    obj.sound()
+    button.onClick()
+    println("--------------------")
+
+
+    // obj within a class
+    println("obj within a class")
+    Myclass.f()// companion
+    Myclass.anotherobj.g()
+    println("--------------------")
+
+// Factory Pattern`
+    println("Factory Pattern")
+    var pizza1= Pizza.create("tomato")
+    println(pizza1)
+    var pizza2= Pizza.create("peppy panner")
+    println(pizza2)
+    println("--------------------")
 
 }
 // object can be created without class
@@ -51,4 +67,35 @@ object sharingWidget{
 //anonymous object
 interface ClickListener {
     fun onClick()
+}
+// Companion Objects
+class Myclass{
+    companion object myobj{// now  we don't need to write myclass.myob.f we can myclass.f()
+    @JvmStatic// IT tell that it behave like a static method in java with this class
+    fun f(){
+
+        println("first object (companion) ")
+    }
+    }
+    object anotherobj{        // it is not a companion object
+        fun g(){
+           println("Another object function")
+        }
+    }
+}
+// Factory Pattern
+class Pizza ( val type: String, val topping: String){
+    companion object Factory{
+        fun create(pizzaType: String): Pizza{// IT NOW return an object
+            return when(pizzaType){
+                " tomato" -> Pizza("Tomato","tomato, cheese")
+                "peppy panner"-> Pizza("panner","panner")
+                else -> Pizza("Basic","onion")
+            }
+
+        }
+    }
+    override fun toString():String{
+        return "Pizza(type=$type,toppings=$topping)"
+    }
 }
