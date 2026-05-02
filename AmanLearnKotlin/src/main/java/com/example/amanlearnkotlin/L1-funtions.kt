b7d0efe82b71abde82ad87ceff37964a14519054
amanzahra12 <amanzahra74@gmail.com>
2026-05-02 14:27:07 +0500

    variables basics

---
+++ AmanLearnKotlin/src/main/java/com/example/amanlearnkotlin/L1-funtions.kt
@@ -0,0 +1,22 @@
+package com.example.amanlearnkotlin
+
+fun main() {
+    /*println("my name is dev aman")
+    println(6+6)
+    println("begining of kotlin learning")
+    //comment
+    /*amanzsjjsjjjdsjdjdjd*/
+var num= 2
+    println(num)
+    var name="aman zahra"
+    var age=20
+    println("name"+name)
+    println("age"+age)*/
+    var name: String
+    name = "John"
+    println(name)
+    val x=5
+    val y=10
+    println(x+y)
+
+}
