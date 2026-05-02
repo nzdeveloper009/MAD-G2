560bc5ebb8ff49ad3407783881059d61668b6849
Hafsa Noor <hafsanoor34202@gmail.com>
2026-05-03 02:24:45 +0500

    Generic class and use of vararg

--- hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/NestedClass.kt
+++ hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/NestedClass.kt
@@ -1,5 +1,7 @@
 package com.example.learnkotlin
 
+import com.sun.jdi.Value
+
 fun main() {
     var obj=Outer.Nested()
     obj.display()
@@ -7,6 +9,16 @@ fun main() {
     // outer constructor call first
     var obj1 = Outer().Inner()
     obj1.display2()
+
+    //with Int
+    var isContainer= Container<Int>(5)
+    isContainer.GetVal()
+
+    //with String
+    var strCont= Container<String>("Hafsa")
+    println(strCont.data)
+
+    Add(1,2,2,3,4,5,6)
 }
 
 class Outer{
@@ -18,7 +30,29 @@ class Outer{
     }
     inner class Inner{
         fun display2(){
-            print("i'm in inner class")
+            println("i'm in inner class")
         }
     }
+}
+
+
+// general container se kisi bhi type ki class bna skty hein just type specify kr k
+// classes with same logic but different data types
+class Container<T>(var data:T){
+    fun SetVal(value: T){
+        data = value
+    }
+
+    fun GetVal():T{
+        return data
+    }
+}
+
+//function for vararg
+fun Add(vararg values:Int){
+    var sum=0
+    for(i in values){
+        sum=sum+i
+    }
+    println(sum)
 }
