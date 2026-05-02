a02235493fe5f505d5ac6d676a576c7fb1c72484
amanzahra12 <amanzahra74@gmail.com>
2026-05-02 14:54:20 +0500

    variables and datatypes

---
+++ AmanLearnKotlin/src/main/java/com/example/amanlearnkotlin/variablesdatatypes.kt
@@ -0,0 +1,24 @@
+package com.example.amanlearnkotlin
+fun main(){
+    /*var score = 5
+    score=10
+    val msg = "hello aman"
+    print(msg)
+    println(score)
+//var can be reassigned
+    //val cant be reassigned
+    //data types integer,floating,boolean,character
+    */
+    var score : Int=12
+    var temperature : Double =79.5
+    var ishappy : Boolean = false
+    var alphabet : Char = 'a'
+    var msg: String= "aman is the best"
+    println(score)
+    println(temperature)
+    println(ishappy)
+
+
+
+
+}
