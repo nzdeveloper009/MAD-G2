f9198387b9fd60ad63b68cdc73419934c9e3216e
Hafsa Noor <hafsanoor34202@gmail.com>
2026-05-02 23:48:03 +0500

    companion objects and factory pattern

--- hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/Object-Declaration.kt
+++ hafsa/LearnKotlin/src/main/java/com/example/learnkotlin/Object-Declaration.kt
@@ -38,4 +38,49 @@ fun main() {
 
     tempDB.connect()
     tempDB.runQuery()
+
+
+    println("obj within a class")
+    Myclass.f()// companion
+    Myclass.anotherobj.g()
+
+
+    println("Factory Pattern")
+    var pizza1= Pizza.create("tomato")
+    println(pizza1)
+    var pizza2= Pizza.create("Tikka")
+    println(pizza2)
+}
+
+
+// Companion Objects
+class Myclass{
+    companion object myobj{
+    @JvmStatic// static method in java
+    fun f(){
+
+        println("first object (companion) ")
+    }
+    }
+    object anotherobj{        //not a companion object
+        fun g(){
+            println("Another object function")
+        }
+    }
+}
+// Factory Pattern
+class Pizza ( val type: String, val topping: String){
+    companion object Factory{
+        fun create(pizzaType: String): Pizza{//return object
+            return when(pizzaType){
+                " tomato" -> Pizza("Tomato","tomato, cheese")
+                "fajita"-> Pizza("fajita","jo bhi")
+                else -> Pizza("Basic","onion")
+            }
+
+        }
+    }
+    override fun toString():String{
+        return "Pizza(type=$type,toppings=$topping)"
+    }
 }
