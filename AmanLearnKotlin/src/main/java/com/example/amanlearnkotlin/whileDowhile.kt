ba91bde2161cc4bbafbadb3011d2728990dc368a
amanzahra12 <amanzahra74@gmail.com>
2026-05-03 18:55:03 +0500

    while do while loop

---
+++ AmanLearnKotlin/src/main/java/com/example/amanlearnkotlin/whileDowhile.kt
@@ -0,0 +1,35 @@
+package com.example.amanlearnkotlin
+
+fun main(){
+    /*var count=5
+    //while loop
+    while(count>=1){
+        println("hello aman jee")
+        count-- //agr dec ki statment ni likhy gy to statment kabhi kam hi nai hogi
+        //to prevent infinite loop
+
+    }
+
+     */
+    //while loop pehly condition check hogi jab condition true hogi to code ecxceute hoga
+    /*var number =2
+    var index=1
+    while(index<=10){
+        println(number*index)
+        index++
+    }
+    println("outside loop-"+index) //condition false condition exceeded
+
+//do while pehly program execute hoga than condition check hogi
+
+
+
+     */
+    var index=6
+    do{
+
+        println("hello")
+        index++ //infinite loop
+    }
+        while(index>5)
+}
