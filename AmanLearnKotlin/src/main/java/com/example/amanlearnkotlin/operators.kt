ea46382dfb67307de37039b1e99b5879bed25906
amanzahra12 <amanzahra74@gmail.com>
2026-05-03 17:55:22 +0500

    operators

---
+++ AmanLearnKotlin/src/main/java/com/example/amanlearnkotlin/operators.kt
@@ -0,0 +1,38 @@
+package com.example.amanlearnkotlin
+fun main() {
+//operators (maths)
+    var i = 15
+    var j = 2
+    println(i + j)
+    println(i - j)
+    println(i * j)
+    println(i.toFloat() / j) //int to float
+        // modulus operator %  remaibder 13%2=1
+    println(i%j)
+    //relational operators > < >= <= == !=
+    println (i > j)
+    println (i<j)
+    println (i>=j)
+    println (i<=j)
+    println (i==j)
+    println (i!=j)
+
+var k=19
+    k =k+1
+println(k)
+    // increment
+    var l = 22
+        //l++
+    //decrement
+
+    //post and pre incrent decrement
+    //++l
+// post increment phly variable ki value ko use krleta hy l++, phly memoery meun ja kr increment kr deta
+
+    //println(l++)
+    //pre inc pehly inc krta phr use krta
+    println(++l)
+
+    var z =7
+    println(z++ + ++z) //7+9
+}
