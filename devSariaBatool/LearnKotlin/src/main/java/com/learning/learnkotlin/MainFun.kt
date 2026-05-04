142cde57b563ef8e660fd83d027080821ebebee3
Saria Batool <batoolsaria8@gmail.com>
2026-05-04 22:07:08 +0500

    LearnKotlin module, main function

---
+++ devSariaBatool/LearnKotlin/src/main/java/com/learning/learnkotlin/MainFun.kt
@@ -0,0 +1,6 @@
+package com.learning.learnkotlin
+
+fun main() {
+    println("Hello World!")
+    println("New Line Print")
+}
---
+++ devSariaBatool/LearnKotlin/src/main/java/com/learning/learnkotlin/MyClass.kt
@@ -0,0 +1,4 @@
+package com.learning.learnkotlin
+
+class MyClass {
+}
