5bdb0d388a0f57fd1336a880919d02e52945ad8a
Saria Batool <batoolsaria8@gmail.com>
2026-05-05 15:19:58 +0500

    LearnKotlin module, variables

---
+++ devSariaBatool/LearnKotlin/src/main/java/Variable.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var score=5
+    score=10
+    val message = "Hello world"
+    println(message)
+    println(score)
+}
+fun mySecondFunction() {
+var score : Int = 32
+var temperature: Double=89.3
+var isRaining: Boolean=false
+var alphabet: Char='A'
+var message: String="Hello Word"
+}
+
+//var can be reassigned
+// val can't be reassigned,used for constent
+//integer(byte,short,int,long)
+//type inference
